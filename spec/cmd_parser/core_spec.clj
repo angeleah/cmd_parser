ff258b1d6135fecccd16572f9b4aa3e50dd24f13
Angeleah Daidone <angeleah@angeleah.com>
2012-11-28 18:02:16 -0600

    Push so I can show Doug.  In a broken state.

--- spec/cmd_parser/core_spec.clj
+++ spec/cmd_parser/core_spec.clj
@@ -1,18 +1,23 @@
 (ns cmd_parser.core-spec
   (:use [speclj.core] [cmd_parser.core]))
 
-;(describe "decoding input sent into the parser"
+(describe "decoding structured messages"
+
+	(it "should return a list with an id and a message when it receives a string"
+		(should= (5 "cool") (decode_structured_message "5, cool;")))
 
 ;	(it	"should return 'i can not parse that' if it is not passed a string."
 ;		(should= "I can not parse that." parse(72)))
+)
 
-;	(it "should be able to parse the id 5 from the message."
-;		(should= [5] parse("5, hello;")))
-;)
+(describe "creating a structured messages"
 
-(describe "creating a structured message"
-
-	(it "should be able to create a structured message when passed a command_id and a message"
+	(it "should be able to create a single structured message in a list when passed an in range command_id and a message"
 		(should= "5, cool;" (create_structured_message 5, "cool")))
+		
+	(it "should return 'The number needs to be within range of 0 - 255.' when passed an out of range command_id"
+		(should= "The number needs to be within range of 0 - 255." (create_structured_message 387, "cool")))
+		
+;	(it "should be able to create multiple structured messages when passed a list of two element lists")
 )
 
--- src/cmd_parser/core.clj
+++ src/cmd_parser/core.clj
@@ -4,7 +4,19 @@
 ;	cond())
 	
 (defn create_structured_message [command_id, message]
-	(apply str [command_id,","" "message ";"]))
+	(if(and (>= command_id 0) (<= command_id 255))
+	(apply str [command_id,","" "message ";"])"The number needs to be within range of 0 - 255."))
+
+(defn decode_structured_message [structured_message]
+	(if(and (>= (re-find #"\d+" structured_message) 0) (<= (re-find #"\d+" structured_message) 255))
+	(list (re-find #"\d+" structured_message))))
+	;then test message part does not have , or ; if it does not,
+	;then conj it onto the list
 	
 	
-; to take in multiple messages, get a count, then do for each? on count? 
+	;find a way to create_st_message when passed multiples(()()())
+	;and to decode when passed multiples
+	
+	
+	
+	;then base 64 encoding?
