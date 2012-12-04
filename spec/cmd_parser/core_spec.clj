578f16035dcf172abc2a06414dc1d8a9aba0662f
Angeleah Daidone <angeleah@angeleah.com>
2012-12-04 11:04:24 -0600

    Add ability to create multiple structured messages. 12 examples, 0 failures.

--- spec/cmd_parser/core_spec.clj
+++ spec/cmd_parser/core_spec.clj
@@ -2,9 +2,10 @@
   (:use [speclj.core] [cmd_parser.core]))
 
 (describe "decoding structured message"
-
-	(it "should return a list with an id and a message when it receives a string"
-		(should= "hello" (decode-structured-message "5, cool;")))
+	
+	(it "should return a list with an id and a message when passed a string"
+		(should= '(5, "cool")
+		 (decode-structured-message "5, cool;")))
 
 	(it	"should return 'The number needs to be within range of 0 - 255.' if the number is out of range."
 		(should-throw Exception "The number needs to be within range of 0 - 255." (decode-structured-message "478, cool;")))
@@ -16,7 +17,7 @@
 (describe "decoding structured messages"
 
 ;	(it "should return a list with two element lists that contain an id and a message when it receives a string"
-;		(should= (5 "cool") (decode-structured-messages "5, cool; 7, sweet; 12, what; 14, OMG;")))
+;		(should= '((5 "cool")(7, "sweet")(12, "what")(14, "OMG")) (decode-structured-messages "5, cool; 7, sweet; 12, what; 14, OMG;")))
 
 ;	(it	"should return 'The number needs to be within range of 0 - 255.' if the number is out of range."
 ;		(should-throw Exception "The number needs to be within range of 0 - 255." (decode-structured-messages "478, cool;")))
@@ -46,16 +47,16 @@
 		(should= "5, cool;" (create-structured-messages '((5, "cool")))))
 		
 	(it "should throw an exception when passed a number out of the range of 0 - 255."
-		(should-throw Exception "The number needs to be within range of 0 - 255." (create-structured-messages '((387, "cool")))))
+		(should-throw Exception  (create-structured-messages '((387, "cool")))))
 
 	(it "should throw an exception when passed something that is NAN as the command id."
-		(should-throw Exception "The command id needs to be a number." (create-structured-messages '(("a", "cool")))))
+		(should-throw Exception (create-structured-messages '(("a", "cool")))))
 		
 	(it "should throw an exception if the message is not a string."
-		(should-throw Exception "The message needs to be a string." (create-structured-messages '((12, 16)))))
+		(should-throw Exception (create-structured-messages '((12, 16)))))
 
-;	(it "should be able to create a list of multiple structured messages when passed a list of two element lists that are formatted properly"
-;		(should= "5, cool; 7, sweet; 12, what; 14, OMG;" (create-structured-messages '((5, "cool")(7, "sweet")(12, "what?")(14, "OMG")))))
+	(it "should be able to create a list of multiple structured messages when passed a list of two element lists that are formatted properly"
+		(should= "5, cool; 7, sweet; 12, what?; 14, OMG;" (create-structured-messages '((5, "cool")(7, "sweet")(12, "what?")(14, "OMG")))))
 )
 
 
--- src/cmd_parser/core.clj
+++ src/cmd_parser/core.clj
@@ -2,19 +2,24 @@
 	(:use [clojure.string]))
 
 (defn decode-structured-message [structured_message]
-	)
-	
-	
 	(let [stripped-message (replace structured_message #";" "") ]
-	
-;		(let [id (read-string (first(split stripped-message #", "))) message (first (rest (split stripped-message #", ")))]
-;			(cond
-;				(not(integer? id))(throw (Exception. "The command id needs to be a number."))
-;				(or(< id 0) (> id 255))(throw (Exception. "The number needs to be within range of 0 - 255."))
-;				(and(>= id 0) (<= id 255)) (vector id message)))))
+		(let [id (read-string (first(split stripped-message #", "))) message (first (rest (split stripped-message #", ")))]
+			(cond
+				(not(integer? id))(throw (Exception. "The command id needs to be a number."))
+				(or(< id 0) (> id 255))(throw (Exception. "The number needs to be within range of 0 - 255."))
+				(and(>= id 0) (<= id 255)) (list id message)))))
 
-(defn decode-structured-messages [messages]
-	messages)
+(defn decode-structured-messages [input]
+	(first(list input)))
+	;turning the input to a list makes the whole thing a one element list
+	;first calls the whole thing
+	;need to break up the elements before I can manipulate them.
+	
+	
+	;		(cond
+	;			(not(integer? id))(throw (Exception. "The command id needs to be a number."))
+	;			(or(< id 0) (> id 255))(throw (Exception. "The number needs to be within range of 0 - 255."))
+	;			(and(>= id 0) (<= id 255)) (list id message)))))
 
 (defn create-structured-message [command_id, message]
 		(cond 
@@ -23,26 +28,10 @@
 			(integer? message)(throw (Exception. "The message needs to be a string."))
 			(and(and (>= command_id 0) (<= command_id 255)) (string? message))(apply str [command_id,","" "message ";"])))
 
+(defn create-structured-message-from-pair [element]
+	(create-structured-message (first element) (first(rest element))))
+
 (defn create-structured-messages [info]
-	(let [data (first info)]
-		(cond
-			(string? (first data))(throw (Exception. "The command id needs to be a number."))
-			(or(< (first data) 0) (> (first data) 255))(throw (Exception. "The number needs to be within range of 0 - 255."))
-			(integer? (first(rest data)))(throw (Exception. "The message needs to be a string."))
-			(and (>= (first data) 0) (<= (first data) 255)) (apply str[(first data),", ", (first(rest data)), ";"]))))
-
-
-	;then call itself or use a loop recur, or just lazy seq but what bout breaking out when exceptions are thrown?
-
-	;	(create_structured_messages (rest(list))
-	;	)
-	;			(if(and (>= command_id 0) (<= command_id 255))
-	;		:find a way to(add result to a list (apply str [command_id,","" "message ";"])(throw (Exception. "The number needs to be within range of 0 - 255."))))))
-			;pass the rest of the list to create_structured_messages
-	;	)
-
-	;then conj it onto the list similar to prime factors or fibb numbers. 
-	;you can do the first and then the rest.
-	
+	 (join " "(map create-structured-message-from-pair info)))
 	
 	;then base 64 encoding?
