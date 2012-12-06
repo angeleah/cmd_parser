1fe13af70a0f4722d3a9a3d189ce21c65c168b60
Angeleah Daidone <angeleah@angeleah.com>
2012-12-06 16:07:24 -0600

    Add ability to pass a parameter of no-encoding or base-64 to creating structured messages.

--- spec/cmd_parser/core_spec.clj
+++ spec/cmd_parser/core_spec.clj
@@ -1,16 +1,19 @@
 (ns cmd_parser.core-spec
   (:use [speclj.core] [cmd_parser.core]))
-
+(comment
 (describe "decoding structured message"
-	
+
 	(it "should return a list with an id and a message when passed a string"
-		(should= '(5, "cool")(decode-structured-message "5, cool;")))
+		(should= '(5, "cool") (decode-structured-message "5, cool;")))
 
 	(it	"should return 'The number needs to be within range of 0 - 255.' if the number is out of range."
 		(should-throw Exception "The number needs to be within range of 0 - 255." (decode-structured-message "478, cool;")))
 
 	(it "should throw an exception when passed something that is NAN as the command id."
 		(should-throw Exception "The command id needs to be a number." (decode-structured-message "a, super cool time;")))
+
+	(it "should be able to decode a single message that is base 64 encoded"
+		(should= '(8, "hello") (decode-structured-message "8, aGVsbG8=;")))
 )
 
 (describe "decoding multiple structured messages"
@@ -23,39 +26,49 @@
 
 	(it "should throw an exception when passed something that is NAN as the command id."
 		(should-throw Exception (decode-structured-messages "a, super cool time;")))
-)
 
+	(it "should return a list with two element lists that contain an id and a message when it receives a string that has base 64 encoded messages"
+		(should= '((5, "cool")(7, "sweet")(12, "what")(14, "OMG")) (decode-structured-messages "5, Y29vbA==; 7, c3dlZXQ=; 12, d2hhdA==; 14, T01H;")))
+)
+)
 (describe "creating a single structured message"
 
 	(it "should be able to create a single structured message in a list when passed an in range command_id and a message"
-		(should= "5, cool awesome sweet;" (create-structured-message 5, "cool awesome sweet")))
+		(should= "5, cool awesome sweet;" (create-structured-message 5, "cool awesome sweet", no-encoding)))
 
 	(it "should throw an exception when passed a number out of the range of 0 - 255."
-		(should-throw Exception "The number needs to be within range of 0 - 255." (create-structured-message 387, "cool")))
+		(should-throw Exception "The number needs to be within range of 0 - 255." (create-structured-message 387, "cool", no-encoding)))
 		
 	(it "should throw an exception when passed something that is NAN as the command id."
-		(should-throw Exception "The command id needs to be a number." (create-structured-message "a", "cool")))
+		(should-throw Exception "The command id needs to be a number." (create-structured-message "a", "cool", no-encoding)))
 		
 	(it "should throw an exception if the message is not a string."
-		(should-throw Exception "The message needs to be a string." (create-structured-message 12, 16)))
+		(should-throw Exception "The message needs to be a string." (create-structured-message 12, 16, no-encoding)))
+		
+	(it "should be able to create a base 64 encoded message"
+		(should= "5, aGVsbCB5ZWFoIHRoaXMgaXMgYmFzZSA2NCBlbmNvZGVk;" (create-structured-message 5, "hell yeah this is base 64 encoded", base-64)))
 )
 
 (describe "creating multiple structured messages at once"
 
 	(it	"should be able to create a one element list with a structured message when passed list containing one list"
-		(should= "5, cool;" (create-structured-messages '((5, "cool")))))
-		
+		(should= "5, cool;" (create-structured-messages [[5, "cool", no-encoding]])))
+
 	(it "should throw an exception when passed a number out of the range of 0 - 255."
-		(should-throw Exception  (create-structured-messages '((387, "cool")))))
+		(should-throw Exception  (create-structured-messages [[387, "cool", no-encoding]])))
 
 	(it "should throw an exception when passed something that is NAN as the command id."
-		(should-throw Exception (create-structured-messages '(("a", "cool")))))
-		
-	(it "should throw an exception if the message is not a string."
-		(should-throw Exception (create-structured-messages '((12, 16)))))
+		(should-throw Exception (create-structured-messages [["a", "cool", no-encoding]])))
 
-	(it "should be able to create a list of multiple structured messages when passed a list of two element lists that are formatted properly"
-		(should= "5, cool; 7, sweet; 12, what?; 14, OMG;" (create-structured-messages '((5, "cool")(7, "sweet")(12, "what?")(14, "OMG")))))
+	(it "should throw an exception if the message is not a string."
+		(should-throw Exception (create-structured-messages [[12, 16, no-encoding]])))
+
+	(it "should be able to create a string of multiple structured messages when passed a list of two element lists that are formatted properly"
+		(should= "5, cool; 7, sweet; 12, what?; 14, OMG;" (create-structured-messages [[5, "cool", no-encoding][7, "sweet", no-encoding][12, "what?", no-encoding][14, "OMG",no-encoding]])))
+
+	(it "should be able to create a string of multiple structured messages when passed a list of two element lists that are formatted with mixed encoding"
+		(should= "5, Y29vbA==; 7, sweet; 12, d2hhdD8=; 14, OMG;" (create-structured-messages [[5, "cool", base-64][7, "sweet", no-encoding][12, "what?", base-64][14, "OMG",no-encoding]])))
+	
 )
 
 (describe "create and decode base 64 encoded messages"
@@ -65,9 +78,10 @@
 
 	(it "should decode a message"
 		(should= "hello" (decode-base-64 "aGVsbG8=")))
-		
+
+;	(it "should return a message that was not encoded"
+;		(should= "haha" (decode-base-64 "haha")))
+
 	( it "should return a message that was passed in"
 		(should= "hello" (no-encoding "hello")))
 )
-
-
--- src/cmd_parser/core.clj
+++ src/cmd_parser/core.clj
@@ -13,32 +13,33 @@
 
 (defn decode-structured-messages [input]
 	(let [result (map decode-structured-message (split input #";"))]
-		(take (count result) result)))
+		(doall result)))
 
-(defn create-structured-message [command_id, message]
+(defn create-structured-message [command_id, message, encoding]
+(prn command_id message encoding)
 		(cond 
 			(string? command_id)(throw (Exception. "The command id needs to be a number."))
 			(or(< command_id 0) (> command_id 255))(throw (Exception. "The number needs to be within range of 0 - 255."))
 			(integer? message)(throw (Exception. "The message needs to be a string."))
-			(and(and (>= command_id 0) (<= command_id 255)) (string? message))(apply str [command_id,","" "message ";"])))
+			(and(and (>= command_id 0) (<= command_id 255)) (string? message))(apply str [command_id,", " (encoding message) ";"])))
 
-(defn create-structured-message-from-pair [element]
-	(create-structured-message (first element) (first(rest element))))
+(defn create-structured-message-from-list [element]
+	(create-structured-message (first element) (first(rest element)) (last element)))
 
 (defn create-structured-messages [info]
-	 (join " "(map create-structured-message-from-pair info)))
-	
+	 (join " "(map create-structured-message-from-list info)))
+
 (defn base-64 [message]
 	 (String. (encode (.getBytes message))))
 	;http://clojure-log.n01se.net/date/2012-10-09.html
-	
-(defn decode-base-64 [message]
-	 (String. (decode (.getBytes message))))
-	
+
 (defn no-encoding [message]
 	message)
-	
+
+(defn decode-base-64 [message]
+	 (String. (decode (.getBytes message))))
+
+
 ;have the create structured message function - it will take id, message and encoding(which will be a funtion)
-;then I will have two functions - b64 which will take a message and encode it.  and another none which will take a 
-;message and just return it.
+
 ;then I can call create structured message and pass it either none or b64
