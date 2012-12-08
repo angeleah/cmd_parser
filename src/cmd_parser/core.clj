9f2107cce3642886c0de4255b8ce22a678e5750e
Angeleah Daidone <angeleah@angeleah.com>
2012-12-08 15:49:20 -0600

    Add ability to decode messages when passed a type of decoding. If no encoding passed, defaults to plain text.

--- src/cmd_parser/core.clj
+++ src/cmd_parser/core.clj
@@ -1,21 +1,44 @@
 (ns cmd_parser.core
-	(:use [clojure.string]
-		  [clojure.data.codec.base64 :as b64]
-		  [clojure.java.io :as io]))
+	(:use
+		[clojure.string :as str :only [split join]])
+	(:require
+		[clojure.data.codec.base64 :as b64 ]
+		[clojure.java.io :as io]))
 
-(defn decode-structured-message [structured_message]
-	(let [stripped-message (replace structured_message #";" "") ]
+(defn base-64 [message]
+	 (String. (b64/encode (.getBytes message))))
+	;http://clojure-log.n01se.net/date/2012-10-09.html
+
+(defn no-encoding [message]
+	message)
+
+(defn plain-text [message]
+	message)
+
+(defn decode-base-64 [message]
+	 (String. (b64/decode (.getBytes message))))
+
+(defn decode-structured-message
+	([structured_message] (decode-structured-message structured_message plain-text))
+	([structured_message decoder]
+	(let [stripped-message (str/replace structured_message #";" "") ]
 		(let [id (read-string (first(split stripped-message #", "))) message (first (rest (split stripped-message #", ")))]
 			(cond
 				(not(integer? id))(throw (Exception. "The command id needs to be a number."))
-				(or(< id 0) (> id 255))(throw (Exception. "The number needs to be within range of 0 - 255."))
-				(and(>= id 0) (<= id 255)) (list id message)))))
+				(or (< id 0)(> id 255)) (throw (Exception. "The number needs to be within range of 0 - 255."))
+				(and(>= id 0) (<= id 255))(list id (decoder message)))))))
 
-(defn decode-structured-messages [input]
+(defn decode-structured-messages
+	([input] (decode-structured-message input plain-text))
+	([input decoder]
 	(let [result (map decode-structured-message (split input #";"))]
-		(doall result)))
+		(doall result))))
 
-(defn create-structured-message [command_id, message, encoding]
+		(defn decode-structured-messages [input decoder]
+			(let [result (map (fn [structured-message] (decode-structured-message structured-message decoder )) (split input #";"))]
+				(doall result)))
+
+(defn create-structured-message [command_id message encoding]
 (prn command_id message encoding)
 		(cond 
 			(string? command_id)(throw (Exception. "The command id needs to be a number."))
@@ -28,18 +51,3 @@
 
 (defn create-structured-messages [info]
 	 (join " "(map create-structured-message-from-list info)))
-
-(defn base-64 [message]
-	 (String. (encode (.getBytes message))))
-	;http://clojure-log.n01se.net/date/2012-10-09.html
-
-(defn no-encoding [message]
-	message)
-
-(defn decode-base-64 [message]
-	 (String. (decode (.getBytes message))))
-
-
-;have the create structured message function - it will take id, message and encoding(which will be a funtion)
-
-;then I can call create structured message and pass it either none or b64
